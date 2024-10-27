e8487a62c53131608d31b783158c23d33567f031
EnzoDPrado <enzodp1501@gmail.com>
2024-10-27 15:33:04 -0300

    feature: user smc controller done

--- src/componentes/card/buy-smc-card/buy-smc-card.jsx
+++ src/componentes/card/buy-smc-card/buy-smc-card.jsx
@@ -1,18 +1,35 @@
 import styles from './buy-smc-card.module.css';
 import plusIcon from '../../../imgs/plus.png';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 const BuySmcCard = () => {
-  const [smc, setSmc] = useState(5)
+  const [smc, setSmc] = useState(() => {
+    try{
+      const smc = localStorage.getItem('smc')
+      return smc ? parseFloat(smc) : 0
+    }catch(error){
+      return 0
+    }
+  })
 
+  const atributeValueOnSmc = (value) => {
+    const actualSmcQuantity = parseFloat(localStorage.getItem('smc'))
+    setSmc(actualSmcQuantity + value)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('smc', smc);
+  }, [smc]);
   
+
   return (
     <div className={styles.content}>
       <p>
         {smc} SMC
       </p>
-      <div className={styles.plus}>
+      <div className={styles.plus} onClick={() => atributeValueOnSmc(500)}>
         <img src={plusIcon} alt="plus button" />
       </div>
     </div>
